e575488ce20d86a415eb72f5466a8ffcb946c62c
Wendy Knudson <ladyknudson@outlook.com>
2021-10-25 16:14:17 -0700

    Passport

--- models/user.js
+++ models/user.js
@@ -1,20 +1,14 @@
 const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema =  new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
     admin: {
         type: Boolean,
         default: false
     }
 });
+
+userSchema.plugin(passportLocalMongoose); // plugs the plugin into userSchema to allow auto authentication related methods
 
 module.exports = mongoose.model('User', userSchema);
