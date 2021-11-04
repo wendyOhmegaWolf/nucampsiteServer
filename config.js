4ce1f2301bd524002921892c6f733914f7744d5c
Wendy Knudson <ladyknudson@outlook.com>
2021-11-04 08:09:05 -0700

    Passport Facebook: WIP

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '585221562791703',
+        clientSecret: 'd1b84a116309c5d8b6c1d2a0b27fb9ea'
+    }
 }
